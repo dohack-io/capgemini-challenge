495e3de64378df6f208486b9ffd66d55b717acfd
Johannes Teklote <johannes.teklote@adesso.de>
2021-05-06 11:40:20 +0200

    update config

---
+++ frontend/src/i18n/de/index.js
@@ -0,0 +1,10 @@
+export default {
+  failed: 'Action failed',
+  success: 'Action was successful',
+  userStatisticsForm: {
+    title: 'Statistik eintragen',
+    coffee: {
+      label: 'Wie viel Kaffee hast du getrunken?'
+    }
+  }
+}
