ea16173427cd6ea9049bf436b0fc703d8e1702a8
Kai <splitt3r@users.noreply.github.com>
2021-05-06 18:55:15 +0000

    fix gradle cmd

--- backend/build-and-push.cmd
+++ backend/build-and-push.cmd
@@ -1,4 +1,4 @@
-./gradle build
+./gradlew.cmd build
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 809541497932.dkr.ecr.eu-central-1.amazonaws.com
 docker build -t capgemini-challenge .
 docker tag capgemini-challenge:latest 809541497932.dkr.ecr.eu-central-1.amazonaws.com/capgemini-challenge:latest
