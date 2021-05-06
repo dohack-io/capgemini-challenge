8c95584fe386600869b9a96285a34887c93aab08
Johannes Teklote <johannes.teklote@adesso.de>
2021-05-06 18:25:05 +0200

    fix usersettings

--- frontend/src/pages/UserSettings.vue
+++ frontend/src/pages/UserSettings.vue
@@ -1,46 +1,45 @@
 <template>
   <q-page class="q-ma-lg">
-    <q-form ref="myForm" @submit="onSubmit">
-      <div class="text-h4 text-no-wrap">{{ $t('userSettings.title') }}</div>
+    <div class="text-h4 text-no-wrap">{{ $t('userSettings.title') }}</div>
 
-      <div class="row q-mt-lg">
-        <q-input class="col" outlined type="email" :label="$t('userSettings.email.label')" v-model="email"/>
-      </div>
+    <div class="row q-mt-lg">
+      <q-input class="col" outlined type="email" :label="$t('userSettings.email.label')" v-model="email"/>
+    </div>
 
-      <div class="row q-mt-lg">
-        <q-input class="col" outlined type="email" :label="$t('userSettings.displayName.label')" v-model="displayName"/>
-      </div>
+    <div class="row q-mt-lg">
+      <q-input class="col" outlined type="email" :label="$t('userSettings.displayName.label')" v-model="displayName"/>
+    </div>
 
-      <div class="row q-mt-lg">
-        <q-input class="col" outlined type="email" :label="$t('userSettings.seat.label')" v-model="seat"/>
-      </div>
+    <div class="row q-mt-lg">
+      <q-input class="col" outlined type="email" :label="$t('userSettings.seat.label')" v-model="seat"/>
+    </div>
 
-      <div class="row q-mt-lg">
-        <q-select class="col" outlined :label="$t('userSettings.defaultCommuteType.label')"
-                  v-model="defaultCommuteType" :options="commuteTypes">
-          <template v-slot:selected-item="props">
-            <q-icon :name="props.opt.icon"/>
-            {{$t(props.opt.label)}}
-          </template>
+    <div class="row q-mt-lg">
+      <q-select class="col" outlined :label="$t('userSettings.defaultCommuteType.label')"
+                v-model="defaultCommuteType" :options="commuteTypes">
+        <template v-slot:selected-item="props">
+          <q-icon :name="props.opt.icon"/>
+          {{$t(props.opt.label)}}
+        </template>
 
-          <template v-slot:option="scope">
-            <q-item v-bind="scope.itemProps" v-on="scope.itemEvents">
-              <q-item-section avatar>
-                <q-icon :name="scope.opt.icon" />
-              </q-item-section>
+        <template v-slot:option="scope">
+          <q-item v-bind="scope.itemProps" v-on="scope.itemEvents">
+            <q-item-section avatar>
+              <q-icon :name="scope.opt.icon" />
+            </q-item-section>
 
-              <q-item-section>
-                <q-item-label v-html="$t(scope.opt.label)"/>
-              </q-item-section>
-            </q-item>
-          </template>
-        </q-select>
-      </div>
+            <q-item-section>
+              <q-item-label v-html="$t(scope.opt.label)"/>
+            </q-item-section>
+          </q-item>
+        </template>
+      </q-select>
+    </div>
 
-      <div class="row q-mt-lg">
-        <q-input class="col" outlined type="number" :label="$t('userSettings.defaultCommuteDistance.label')"
-                 v-model.number="defaultCommuteDistance"/>
-      </div>
+    <div class="row q-mt-lg">
+      <q-input class="col" outlined type="number" :label="$t('userSettings.defaultCommuteDistance.label')"
+               v-model.number="defaultCommuteDistance"/>
+    </div>
 
     <div class="row q-mt-lg">
       <q-btn unelevated tile outline :label="$t('userSettings.cancel')" @click="$router.push('/')" color="primary" class="q-mr-sm"/>
